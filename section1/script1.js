c24849ca834ebfe793a5822d45b004238a746598
stomaras <sptomaras@yahoo.gr>
2019-11-13 17:59:19 +0200

    Create script1.js

---
+++ section1/script1.js
@@ -0,0 +1,16 @@
+var firstName = 'Spyros';
+console.log(firstName);
+
+var lastName = 'Tomaras';
+var age = 22;
+
+var fullAge = true;
+
+console.log(fullAge);
+
+var job;
+
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
