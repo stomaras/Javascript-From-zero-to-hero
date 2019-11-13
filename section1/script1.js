32f0a85fe5bfc6b9492b0628311e96bcc4931fa2
stomaras <sptomaras@yahoo.gr>
2019-11-13 11:47:44 -0500

    Add files via upload

--- section1/script1.js
+++ section1/script1.js
@@ -42,3 +42,27 @@ alert(firstName + ' is a ' + age + ' years old ' + job + '. Is he Married? ' +  
 
 var lastName  = prompt('What is his last name ?');
 console.log(firstName + ' ' + lastName);
+
+// Basic Operators 
+
+var year, yearTom, yearChris, yearKostas;
+year = 2019;
+yearTom = year - 1997;
+yearChris = year - 1997;
+yearKostas = year - 1998;
+console.log(' Tom is : ' + yearTom);
+console.log(' Chris is : ' + yearChris);
+console.log(' Kostas is : ' + yearKostas);
+console.log(year / 10);
+
+// Logical Operators 
+
+var tomOlder = yearTom < yearKostas;
+console.log(tomOlder);
+
+// typeof operator
+console.log(typeof tomOlder);
+console.log(typeof yearTom);
+console.log(typeof 'hihihihihi');
+var x;
+console.log(typeof x);
