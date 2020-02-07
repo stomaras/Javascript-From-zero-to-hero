174490015901c75f467d05af5a10a5825b4ec814
stomaras <sptomaras@yahoo.gr>
2020-02-07 12:58:58 +0200

    Update CodingChallenge.js

--- section1/CodingChallenge.js
+++ section1/CodingChallenge.js
@@ -26,7 +26,34 @@ var BMIMark = massMark / (heightMark * heightMark);
 var BMIJohn = massJohn / (heightJohn * heightJohn);
 console.log(BMIMark, BMIJohn);
 
+var firstName = 'John'
+var civilStatus = 'single';
+
+if(civilStatus === 'married'){
+    console.log(firstName + ' is married!');
+}else{
+    console.log(firstName + 'will hopefully marry soon:)');
+}
+
+var isMarried = true;
+if(isMarried){
+    console.log(firstName + ' is married!');
+}else{
+    console.log(firstName + 'will hopefully marry soon:)');
+}
 
 
-var markHigherBMI = BMIMark > BMIJohn;
-console.log('Is Mark\'s BMI higher than John\'s'  + markHigherBMI);
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92; // kg
+var heightJohn = 1.95 // meters
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if(BMIMark > BMIJohn){
+    console.log('Mark\ BMI is higher than John\'s.');
+}else{
+    console.log('John\ BMI is higher than Mark\'s.');
+}
