10f19486cfbd65c45bc604a21449152ebcd27279
stomaras <sptomaras@yahoo.gr>
2020-02-19 14:11:26 +0200

    Update scripting.js

--- section4/scripting.js
+++ section4/scripting.js
@@ -154,3 +154,37 @@ console.log(fullAges);
 console.log(rates);
 
 
+// After functions accepting functions as input let's now talk about functions returning functions
+// Let's create a function that creates different interview questions for different jobs 
+// Anonymous function is a function which does not has a name
+
+// Lecture: Functions returning functions 
+
+function interviewQuestion(job){
+    if(job === 'designer'){
+        return function(name){
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    }else if(job === 'teacher'){
+        return function(name){
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    }else{
+        return function(name){
+            console.log('Hello'+name+'what do you do?');
+        }
+    }
+}
+
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('Mark');
+designerQuestion('Jane');
+
+interviewQuestion('teacher')('Tom');
+
+
+
