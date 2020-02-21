ade15ff0ff126fac79141862c30e7b955a3463ec
stomaras <sptomaras@yahoo.gr>
2020-02-21 15:55:58 +0200

    Update app.js

--- section5/BudgetApp/app.js
+++ section5/BudgetApp/app.js
@@ -1,54 +1,99 @@
+// BUDGET CONTROLLER
+
+// We are going to create an object which will keeps the type description and value 
+// in the UIController so that if i decide to change the UI the changes be locally 
 var budgetController = (function(){
-    
-    var x = 23;
-    
-    var add = function(a){
-        return x + a;
-    }
-    
-    return {
-        // the publicTest method that we exposed to the public 
-        // this works due to the powerful of closures 
-        // because of closures an inner function has always access to the variables and parameters
-        //of its outer function even after the outer function has returned 
-        // x and add variables are private because they are in the closure and therefore only 
-        // the publicTest method can access them 
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
-})();
 
-
-var UIController = (function(){
-    
     // Some code
     
 })();
 
-// Now we create another module with just the one that's gonna take care of our uder interface 
-// And let's call that one the UIcontroller 
-
-// The User Interface is seperated from the data they don't communicate at all 
-// and that is called separation of concerns 
-
-// Separation of Concerns : means that each part of the application should only be interested
-//                          in doing one thing independently so these two controllers don't know
-//                          about each other so we need to connect these with some way 
-//                          so we need to read data from the User Interface and then add the data
-//                          as a new expanse in the budgetController so we create a new controller app
-
-
-// Third module 
-// So this controller knows about the other 2 modules 
-var controller = (function(budgetCtrl, UICtrl) {
+// We are going to select add__type, add_description, add__value and return the type 
+// UI CONTROLLER 
+var UIController = (function(){
     
-    var z = budgetCtrl.publicTest(5);
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    
     
     return {
-        anotherPublic: function(){
-            console.log(z);
+        getinput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+            
+        getDOMstrings: function() {
+            return DOMstrings;
         }
+    };
+    
+})();
+
+
+// GLOBAL APP CONTROLLER 
+var controller = (function(budgetCtrl, UICtrl) {
+    
+    var setupEventListeners = function(){
+    
+        var DOM = UICtrl.getDOMstrings();
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        
+        document.addEventListener('keypress', function(event) {
+       
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });   
+
+    };
+    
+    
+    
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
+        
+        // 2. Add the item to the budget Controller
+        
+        // 3. Add the item to the UI 
+        
+        // 4. Calculate the budget 
+        
+        // 5. Display the budget on the UI 
+        
+        console.log('It works.');
+        
     }
     
+    // I will set up the Event Listener for the input button right here in our controller
+    // .add__btn becaue is class 
+    // Not only the key Event but also the keypress event     
+    // The keypress event does not happen on any specific element but it happens on the global 
+    // web page , anywhere in the document 
+    // Keyboard Event is a user interface event in the prototype chain 
+    // the keyCode identifies the key that we pressed. We can see it in the console
+    // Some older browsers use the which
+    
+    return {
+        init: function(){
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
+    
+    
 })(budgetController,UIController);
+
+// Without this line of code nothing will happen because without EventListeners we cannot get data
+// and without data there is not application.
+controller.init();
