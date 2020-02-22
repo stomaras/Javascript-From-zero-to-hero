2d4df833d6ccf99ad1c2e568153aea0fa1952b5b
stomaras <sptomaras@yahoo.gr>
2020-02-22 15:06:51 +0200

    Update app.js

--- section5/BudgetApp/app.js
+++ section5/BudgetApp/app.js
@@ -109,7 +109,23 @@ var UIController = (function(){
             // as a child of these containers: income__list, expenses__list
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
         },
+        
+        clearFields: function() {
+            var fields, fieldsArr;    
             
+            // querySelectorAll return list 
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);  
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array){
+               current.value = ""; 
+            });
+            
+            fieldsArr[0].focus();
+            
+        },
+        
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -148,9 +164,13 @@ var controller = (function(budgetCtrl, UICtrl) {
         
         // 3. Add the item to the UI 
         UICtrl.addListItem(newItem, input.type);
-        // 4. Calculate the budget 
         
-        // 5. Display the budget on the UI 
+        // 4. Clear the fields 
+        UICtrl.clearFields();
+        
+        // 5. Calculate the budget 
+        
+        // 6. Display the budget on the UI 
         
         console.log('It works.');
         
@@ -160,7 +180,7 @@ var controller = (function(budgetCtrl, UICtrl) {
             console.log('Application has started.');
             setupEventListeners();
         }
-    };
+    }
     
     
 })(budgetController,UIController);
