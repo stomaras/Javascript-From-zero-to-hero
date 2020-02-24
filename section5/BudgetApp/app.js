1224e5893587a9f30cbe87d9e0e8d0a1a7401797
stomaras <sptomaras@yahoo.gr>
2020-02-24 14:26:46 +0200

    Update app.js

--- section5/BudgetApp/app.js
+++ section5/BudgetApp/app.js
@@ -14,6 +14,14 @@ var budgetController = (function(){
         this.value = value;
     };
     
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+           sum += cur.value; 
+        });
+        data.totals[type] = sum;
+    };
+    
     
     var data = {
         allItems: {
@@ -23,7 +31,9 @@ var budgetController = (function(){
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
     
     
@@ -55,6 +65,36 @@ var budgetController = (function(){
             
         },
         
+        calculateBudget: function(){
+          
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            
+            // calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+            
+            //calculate the percentage of income that we spent
+            if (data.totals.inc > 0){
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            }else {
+                data.percentage = -1;
+            }
+            
+            
+            
+            //Expense = 100 and income 200, spent 50% = 100/200 = 0.5 * 100
+        },
+        
+        getBudget: function(){
+          return {
+              budget:data.budget,
+              totalInc:data.totals.inc,
+              totalExp:data.totals.exp,
+              percentage:data.percentage
+          };  
+        },
+        
         testing: function(){
             console.log(data);
         }
@@ -82,7 +122,7 @@ var UIController = (function(){
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
         
@@ -152,29 +192,41 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     };
     
-    
+    var updateBudget = function(){
+        
+        // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
+        
+        // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
+        
+        // 3. Display the budget on the UI 
+        console.log(budget);
+    }
     
     var ctrlAddItem = function() {
         var input, newItem;
+        
         // 1. Get the field input data
         input = UICtrl.getinput();
         
-        // 2. Add the item to the budget Controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        // Here i prevent in order to get valid data  
+        if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+            
+            // 2. Add the item to the budget Controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         
-        // 3. Add the item to the UI 
-        UICtrl.addListItem(newItem, input.type);
+            // 3. Add the item to the UI 
+            UICtrl.addListItem(newItem, input.type);
         
-        // 4. Clear the fields 
-        UICtrl.clearFields();
+            // 4. Clear the fields 
+            UICtrl.clearFields();
         
-        // 5. Calculate the budget 
-        
-        // 6. Display the budget on the UI 
-        
-        console.log('It works.');
-        
-    }
+            // 5. Calculate and update budget 
+            updateBudget(); 
+        }
+    
+    };
     return {
         init: function(){
             console.log('Application has started.');
@@ -186,6 +238,5 @@ var controller = (function(budgetCtrl, UICtrl) {
 })(budgetController,UIController);
 
 controller.init();
-
 
 
