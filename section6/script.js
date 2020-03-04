6f5527fe37db030d575980827e871d3923df5a02
stomaras <sptomaras@yahoo.gr>
2020-03-04 23:21:07 +0200

    Update script.js

--- section6/script.js
+++ section6/script.js
@@ -1,1 +1,56 @@
+// Lecture: let and const 
+/*
+//ES5 
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
 
+//ES6 
+const name6 = 'Jane Smith';
+let age6 = 23;
+name6 = 'Jane Miller';
+//error
+//console.log(name6);
+*/
+
+
+
+
+//ES5 
+function driversLicence5(passedTest){
+    //console.log(firstName);   
+    if(passedTest){
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+driversLicence5(true);
+
+
+//ES6
+function driversLicence6(passedTest){
+    // console.log(firstName);// temporal-dead zone
+    // if we want to use a constant outside of the block then we must to declare out of the block
+    let firstName;
+    const yearOfBirth = 1990;
+    if(passedTest){
+        firstName = 'John';
+        //const yearOfBirth = 1990;
+    }
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+driversLicence6(true);
+
+
+
+
+
+let i = 23;
+
+for (let i=0; i < 5; i++){
+    console.log(i);
+}
+
+console.log(i);
