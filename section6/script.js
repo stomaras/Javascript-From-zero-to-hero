2e5d22f5f4ef735bafadb5cefb33a61eb4eff767
stomaras <sptomaras@yahoo.gr>
2020-03-05 15:24:55 +0200

    Update script.js

--- section6/script.js
+++ section6/script.js
@@ -139,3 +139,36 @@ ages6 = years.map((el, index) => {
     return `Age element ${index + 1}: ${age}.`
 });
 console.log(ages6);
+
+
+function Person(name) {
+    this.name = name;
+}
+
+
+// ES5
+Person.prototype.myFriends5 = 
+function(friends){
+    
+    var arr = friends.map(function(el)
+    {
+        return this.name + 'is friends with ' + el;
+    }.bind(this));
+    
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+
+
+//ES6
+Person.prototype.myFriends6 = 
+function(friends) {
+    
+    var arr = friends.map(el => `${this.name} is friends with ${el}`);
+    console.log(arr);
+}
+
+new Person('Mike').myFriends6(friends);
