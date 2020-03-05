66877f03a725c6ccc0b9baf82aa271c9b952230d
stomaras <sptomaras@yahoo.gr>
2020-03-05 14:38:11 +0200

    Update script.js

--- section6/script.js
+++ section6/script.js
@@ -113,4 +113,29 @@ console.log(fullName.includes(' '));
 console.log(firstName.repeat(3));
 console.log(`${firstName}`.repeat(5));
 
+/////////////////////////////////////
+// Lecture: Arrow functions 
 
+const years = [1990, 1965, 1982, 1937];
+
+
+// ES5 
+var age5 = years.map(function(el) {
+    return 2020 - el;
+});
+console.log(age5);
+
+// ES6 
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age Element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+// more than 1 line of code we add curly braces 
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
