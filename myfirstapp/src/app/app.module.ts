7ed2efbad20d33b19717b8b3bce7fe1f33db7bdc
stomaras <sptomaras@yahoo.gr>
2020-03-22 22:50:54 +0200

    Update app.module.ts

--- myfirstapp/src/app/app.module.ts
+++ myfirstapp/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
