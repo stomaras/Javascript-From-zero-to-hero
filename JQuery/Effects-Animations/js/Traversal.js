16e06cb6937942e6eda5a70393f9053c82475226
stomaras <sptomaras@yahoo.gr>
2020-04-22 22:36:59 +0300

    Create Traversal.js

---
+++ JQuery/Effects-Animations/js/Traversal.js
@@ -0,0 +1,13 @@
+$(function() {
+    //$("#list").find("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+    //$("#list").children("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+    //$("#list").parents("body").css("background-color", "rgba(180, 180, 30, 0.8)");
+    //$("#list").parents("div").css("background-color", "rgba(180, 180, 30, 0.8)");
+    //$("#list").parent("body").css("background-color", "rgba(180, 180, 30, 0.8)");
+    //$("#list").siblings("p").css("background-color", "rgba(180, 180, 30, 0.8)");
+    //$("#list").siblings(":header").css("background-color", "rgba(180, 180, 30, 0.8)");
+    $("#list").prev().css("background-color", "rgba(180, 180, 30, 0.8)");
+    $("#list").next().css("background-color", "rgba(180, 180, 30, 0.8)");
+    //$("#list").prev().next().css("background-color", "rgba(180, 180, 30, 0.8)");
+
+});
