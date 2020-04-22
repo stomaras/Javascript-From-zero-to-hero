4e748f30bf163e5050a414b98e4077c0c2ef1906
stomaras <sptomaras@yahoo.gr>
2020-04-22 03:03:35 +0300

    Create script4.js

---
+++ JQuery/Effects-Animations/js/script4.js
@@ -0,0 +1,5 @@
+$(function() {
+    $(".red-box").fadeTo(1000, 0.2);
+    $(".green-box").delay(1000).fadeTo(1000, 0.5);
+    $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
+});
