6124da571e096096409fe1767ebadd9eefb80371
stomaras <sptomaras@yahoo.gr>
2020-04-26 20:55:33 +0300

    Create AddRemElem.js

---
+++ JQuery/Effects-Animations/js/AddRemElem.js
@@ -0,0 +1,19 @@
+$(function(){
+    /*
+    $("a").addClass("fancy-link");
+    $("p:first").addClass("large emphasize");
+    
+    $("li li").addClass(function(index) {
+        $(this).addClass("item-" + index);
+    });
+    
+    $("div").addClass(function(index, currentClass) {
+        if (currentClass === "dummy") {
+            return "red-box";
+        }
+    });
+    
+    $(".red-box").removeClass("red-box").addClass("blue-box");
+    */
+    $(".dummy").removeClass("dummy").addClass("green-box");
+});
